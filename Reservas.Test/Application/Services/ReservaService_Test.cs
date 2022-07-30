5d1388705d530e22c13cbd810a59bdca79041dc5
gustavito12 <gusavt1989@gmail.com>
2022-07-29 23:26:18 -0400

    Commit de Clone

---
+++ Reservas.Test/Application/Services/ReservaService_Test.cs
@@ -0,0 +1,30 @@
+﻿using Reservas.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Reservas.Test.Application.Services {
+    public class ReservaService_Test {
+        [Theory]
+        [InlineData(4, false)]
+        [InlineData(5, false)]
+        [InlineData(6, false)]
+        [InlineData(8, false)]
+        [InlineData(10, false)]
+        [InlineData(20, false)]
+        [InlineData(7, false)]
+        [InlineData(1, false)]
+        public async void GenerarPedido_CheckValidData(int expectedIdReserva, bool isEqual) {
+            var ReservaService = new ReservaService();
+            int idReserva = await ReservaService.GenerarIdReservaAsync();
+            if (isEqual) {
+                Assert.Equal(expectedIdReserva, idReserva);
+            } else {
+                Assert.NotEqual(expectedIdReserva, idReserva);
+            }
+        }
+    }
+}
